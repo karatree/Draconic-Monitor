b37bf2614fcab5e778e8cf93d216ecd412c7a6e5
Shawn <45434014+karatree@users.noreply.github.com>
2020-08-22 12:44:05 -0400

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,25 @@
+--installation script
+--rhNbNbFE
+--Version 1.0
+
+local libURL = "https://raw.githubusercontent.com/OmegaPhi115/Draconic-Evolution-Reactor-Controller/master/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/karatree/Draconic-Monitor/master/Draconic%20Controller.lua"
+local lib, startup
+local libFile, startupFile
+
+fs.makeDir("lib")
+
+lib = http.get(libURL)
+libFile = lib.readAll()
+
+local file1 = fs.open("lib/f", "w")
+file1.write(libFile)
+file1.close()
+
+startup = http.get(startupURL)
+startupFile = startup.readAll()
+
+local file2 = fs.open("startup", "w")
+file2.write(startupFile)
+file2.close()
+os.reboot()
